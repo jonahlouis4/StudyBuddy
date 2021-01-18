917dbdf3b8c0fb15ae8ca9ae82ab2724209be697
UrLover <jonah_louis@outlook.com>
2021-01-17 20:19:41 -0500

    :heavy_check_mark: Added JSDoc +func for questions

--- src/components/QuestionsModal.js
+++ src/components/QuestionsModal.js
@@ -2,20 +2,30 @@ import React from 'react'
 import Modal from 'react-bootstrap/Modal'
 import { motion } from 'framer-motion'
 
-/* Variants for close button */
+/** Variants for close button */
 const buttonVariants = {
     hover: { scale: 1.1 },
     tap: { scale: 0.9 }
   }
 
+  /**
+   * Modal for viewing and deleting questions + answers that exists
+   * @param {prop} props - props passed from Question.js
+   */
 const QuestionsModal = (props) => {
+    /** Returns list if questions exists or a message if it's empty */
+    const verifyEmpty = () => {
+        if (props.questionlist.length === 0) { return <p>No questions exists.</p> } 
+        else { return props.questionlist }   
+    }
+    
     return (
         <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered >
             <Modal.Header >
                 <Modal.Title id="contained-modal-title-vcenter">My Questions</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <div> { props.questionlist } </div>
+                <div> { verifyEmpty() } </div>
             </Modal.Body>
             <Modal.Footer>
                 <motion.button 
