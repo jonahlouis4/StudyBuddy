5095c0d70f16f56c1d8aefddb8363f5029636a30
UrLover <jonah_louis@outlook.com>
2021-06-02 15:34:44 -0400

    Completed footer look

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+const Footer = () => {
+    const heartIcon = <FontAwesomeIcon icon={faHeart} className="text-danger"/>
+    const githubIcon = <FontAwesomeIcon icon={faGithub} className="text-primary"/>
+    const myFooter = { marginTop: 'auto' }
+
+    return (
+        <footer
+        className="bg-dark py-5 text-white"
+        style={myFooter}
+        >
+            <div
+            className="container"
+            >
+                <div
+                className="row"
+                >
+                    <div
+                    className="col-8"
+                    >
+                        <p
+                        className="text-secondary"
+                        >
+                           Made with { heartIcon } by <a href="https://www.jonahlouis.ca/" target="_blank">Jonah Louis</a> 
+                        </p> 
+                    </div>
+                    <div
+                    className="col-4 text-right"
+                    >
+                        {/*  */}
+                        <a href="https://github.com/jonahlouis4/StudyBuddy" target="_blank">
+                            { githubIcon }
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './Nav'
+import Footer from './Footer'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 
@@ -31,25 +32,31 @@ const Home = () => {
             <div
             className="container py-5"
             >
-                <motion.h2 
+                <motion.h2
+                className="text-primary text-center"
                 variants={fadeIn}
-                style={{textAlign:'center'}}
+                >
+                    Time to study!
+                </motion.h2>
+                <motion.h4 
+                className="text-center"
+                variants={fadeIn}
                 >
                     Select one of the following options
-                </motion.h2>
+                </motion.h4>
                 <motion.div 
                 className="pt-5"
                 variants={fadeIn}
                 >
                     <div 
-                    className="d-grid"
+                    className="d-grid text-center"
                     >
                         <motion.div
                         variants={fadeIn}
                         >
                             <Link to="/questions" className="home--links">
                                 <motion.button 
-                                className="btn btn-lg btn-primary col-12" 
+                                className="btn btn-lg btn-primary col-6" 
                                 type="button"
                                 variants={fadeIn}
                                 whileHover={{ scale: 1.1 }}
@@ -64,7 +71,7 @@ const Home = () => {
                         >
                             <Link to="/quiz" className="home--links">
                                 <motion.button 
-                                className="btn btn-lg btn-primary col-12" 
+                                className="btn btn-lg btn-primary col-6" 
                                 type="button"
                                 variants={fadeIn}
                                 whileHover={{ scale: 1.1 }}
@@ -76,6 +83,7 @@ const Home = () => {
                     </div>
                 </motion.div>
             </div>
+            <Footer />
         </motion.div>
     )
 }
