3cf38e856bbcb12f98ff193024732ab0baa865ff
UrLover <jonah_louis@outlook.com>
2021-06-03 11:15:37 -0400

    Cleaned up code and css

--- src/components/pages/QuizResult.js
+++ src/components/pages/QuizResult.js
@@ -16,24 +16,71 @@ const QuizResult = ({QAcopy, answer, getResult, currQuestion, setQuestionIndex, 
     }
 
     return (
-        <motion.div className="quiz-body" variants={containerVariantsChild} initial="hidden" animate="visible" exit="exit">
+        <motion.div 
+        className="" 
+        variants={containerVariantsChild} 
+        initial="hidden" 
+        animate="visible" 
+        exit="exit"
+        >
             {/* QUESTION */}
-            <div className="quiz-questionBox">
-                <motion.p variants={fadeIn}>{currQuestion+1}/{frame.length}</motion.p>
-                <motion.p variants={fadeIn} className="label">{frame[currQuestion].question}</motion.p>
+            <div 
+            className=""
+            >
+                <motion.p 
+                variants={fadeIn}
+                >
+                    {currQuestion+1}/{frame.length}
+                </motion.p>
+                <motion.p 
+                variants={fadeIn} 
+                className=""
+                >
+                    {frame[currQuestion].question}
+                </motion.p>
             </div>
                 {/* ANSWERS */}
-            <div className="quiz-answerBox">
+            <div 
+            className=""
+            >
                 {/* The answer */}
-                <motion.p variants={fadeIn} className="label">The answer</motion.p>
-                <motion.p variants={fadeIn} >{ frame[currQuestion].answer }</motion.p>
+                <motion.p 
+                variants={fadeIn} 
+                className=""
+                >
+                    The answer
+                </motion.p>
+                <motion.p 
+                variants={fadeIn} 
+                >
+                    { frame[currQuestion].answer }
+                </motion.p>
                 {/* 'Your' answer */}
-                <motion.p variants={fadeIn} className="label">Your answer</motion.p>
-                <motion.p variants={fadeIn} >{ answer.answer }</motion.p>
+                <motion.p 
+                variants={fadeIn} 
+                className="label"
+                >
+                    Your answer
+                </motion.p>
+                <motion.p 
+                variants={fadeIn} 
+                >
+                    { answer.answer }
+                </motion.p>
                 {/* Next */}
-                <motion.div variants={fadeIn} className="Q-btnContainer">
-                    <motion.button variants={buttonVariants} whileHover="hover" 
-                    whileTap="tap" className="main-btn" onClick={handleNext}>next</motion.button>
+                <motion.div 
+                variants={fadeIn} 
+                className="Q-btnContainer"
+                >
+                    <motion.button 
+                    variants={buttonVariants} 
+                    whileHover="hover" 
+                    whileTap="tap" 
+                    className="main-btn" 
+                    onClick={handleNext}
+                    >
+                        next
+                    </motion.button>
                 </motion.div>
             </div>
         </motion.div>
