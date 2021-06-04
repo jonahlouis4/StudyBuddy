ecbb913fb5115b7c0bbaa3819eb42cb5815caea9
UrLover <jonah_louis@outlook.com>
2021-06-04 08:02:30 -0400

    changed import order

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styling/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './styling/App.scss';
-// Easybase
 import { EasybaseProvider } from 'easybase-react';
 import ebconfig from './ebconfig';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styling/App.scss';
+import './styling/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
