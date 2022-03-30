ae4686a80dc0767615494c291b05b4a7921af460
jonahlouis4 <jonah_louis@outlook.com>
2022-03-30 03:22:42 -0400

    Added checkbox in result

--- src/pages/quiz_pages/QuizResult.js
+++ src/pages/quiz_pages/QuizResult.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 const QuizResult = ({QAcopy, answer, getResult, currQuestion, setQuestionIndex, containerVariantsChild, fadeIn, buttonVariants, QA}) => {
     
@@ -52,6 +54,17 @@ const QuizResult = ({QAcopy, answer, getResult, currQuestion, setQuestionIndex, 
                 <motion.h6 variants={fadeIn} style={{fontWeight:400}}>
                     { answer.answer }
                 </motion.h6>
+                <motion.div variants={fadeIn} class="form-check mt-5 mb-3">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Mark as incorrect
+                    </label>
+                    <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">This will mark a question as incorrect so you can review it later.</Tooltip>}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="ml-2" style={{height: 20}} fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" >
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </OverlayTrigger>
+                </motion.div>
                 {/* Next */}
                 <motion.div variants={fadeIn}>
                     <motion.button 
