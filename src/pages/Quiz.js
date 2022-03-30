5c55930fb967f05aaf30e54905199d042a8743ac
jonahlouis4 <jonah_louis@outlook.com>
2022-03-30 01:08:38 -0400

    Adds randomizer

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -91,6 +91,8 @@ const Quiz = () => {
         }
         // Shuffle questions 
         else if (currQuestion === 0 && answer.answer === "") { 
+            QA.sort(function(a, b){ return 0.5 - Math.random() })
+            console.log("QA --> ", QA)
             setResult(ENTER_PAGE); 
         }
     }, [QA.length, currQuestion, answer.answer])
@@ -108,7 +110,7 @@ const Quiz = () => {
         else if (result === ENTER_PAGE) {
             return <QuizEnter getResult={getResult} currQuestion={currQuestion} 
                         addAnswer={addAnswer} containerVariantsChild={containerVariantsChild} 
-                        fadeIn={fadeIn} buttonVariants={buttonVariants} frame={frame}/>
+                        fadeIn={fadeIn} buttonVariants={buttonVariants} frame={QA}/>
         } 
         else if (result === RESULT_PAGE) {
             return <QuizResult getResult={getResult} currQuestion={currQuestion} answer={answer} 
